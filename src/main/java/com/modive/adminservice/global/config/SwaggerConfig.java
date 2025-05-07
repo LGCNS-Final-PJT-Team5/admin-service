fd5f8c54e59ba82528b2755936a871add635af75
jiminh00 <jiminh1012@gmail.com>
2025-05-07 13:49:29 +0900

    chore(#1): Swagger 설정 추가

---
+++ src/main/java/com/modive/adminservice/global/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.modive.adminservice.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Modive Admin Service API Docs",
+                description = "모디브의 관리자 서비스 API 문서입니다.",
+                version = "v1"
+        )
+)
+public class SwaggerConfig {
+    private static final String BEARER_TOKEN_PREFIX = "Bearer ";
+
+    @Bean
+    public OpenAPI openAPI() {
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components()
+                .addSecuritySchemes(securityJwtName, new SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat(securityJwtName));
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+}
