2506968091448184067f9137a66a03bb95718d30
Jimin Hwang <jiminh1012@gmail.com>
2025-06-03 15:54:23 +0900

    test(#17): 리워드 관련 단위 테스트 작성  (#21)
    * chore(#17): 테스트 관련 의존성 추가
    
    * feat(#17): RewardFetcherService 단위 테스트 추가
    
    * feat(#17): 테스트 추가 및 날짜 검증 메소드 추가
    
    - NPE 관련 테스트 코드 추가
    - 날짜 관련 경계값 분석 테스트 코드 추가
    - 날짜 검증 메소드 추가 (유효한 달인지 확인)

--- src/main/java/com/modive/adminservice/global/error/code/ErrorCode.java
+++ src/main/java/com/modive/adminservice/global/error/code/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
     FEIGN_DATA_PARSE_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "요청 응답에서 필드를 파싱하지 못했습니다."),
-    FEIGN_DATA_MISSING(HttpStatus.INTERNAL_SERVER_ERROR, "요청 응답 데이터가 없습니다.");
+    FEIGN_DATA_MISSING(HttpStatus.INTERNAL_SERVER_ERROR, "요청 응답 데이터가 없습니다."),
+
+    INVALID_MONTH_VALUE(HttpStatus.BAD_REQUEST, "월 값이 유효하지 않습니다. 1-12 사이의 값을 입력해주세요.");
 
     private final HttpStatus status;
     private final String message;
